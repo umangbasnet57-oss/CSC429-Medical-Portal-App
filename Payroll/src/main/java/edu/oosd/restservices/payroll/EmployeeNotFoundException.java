e5707abea402753edc1c395fc45f76b02ebc8fbd
Frederick Laud Amoah-Darko Jr. <freddie@Fredericks-MacBook-Pro-2.local>
2026-02-18 13:14:28 -0500

    directories would be deleted

--- Payroll/src/main/java/edu/oosd/restservices/payroll/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.oosd.restservices.payroll;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(Long id) {
-        super("Could not find employee " + id);
-    }
-}
--- Payroll/src/main/java/edu/oosd/restservices/payroll/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package edu.oosd.restservices.payroll;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-}
