33067dd3558d2962abe74a75c740c36c4cac30a6
Frederick Laud Amoah-Darko Jr. <freddie@Fredericks-MacBook-Pro-2.local>
2026-02-17 22:52:09 -0500

    Final repo submission for lab 4.

--- RestApi/src/main/java/edu/oosd/restservices/restapi/Greeting.java
+++ RestApi/src/main/java/edu/oosd/restservices/restapi/Greeting.java
@@ -1,13 +1,17 @@
 package edu.oosd.restservices.restapi;
 
-public class Greeting {
-    long id;
-    String content;
+import org.springframework.hateoas.RepresentationModel;
 
+public class Greeting extends RepresentationModel<Greeting>{
+    long id;
+    String content = "Welcome to Greeting API";
+
+    public Greeting(){}
     public Greeting(long id, String content){
         setId(id);
         setContent(content);
     }
+
 
     public void setId(long id) {
         this.id = id;
