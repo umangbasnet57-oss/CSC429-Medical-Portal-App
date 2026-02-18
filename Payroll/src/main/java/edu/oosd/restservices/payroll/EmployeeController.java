21b77f01efa365c54e64f0530197293c8a7182fa
Frederick Laud Amoah-Darko Jr. <freddie@Fredericks-MacBook-Pro-2.local>
2026-02-18 13:12:34 -0500

    Dealing with inconsitences.

--- Payroll/src/main/java/edu/oosd/restservices/payroll/EmployeeController.java
+++ Payroll/src/main/java/edu/oosd/restservices/payroll/EmployeeController.java
@@ -1,7 +1,9 @@
 package edu.oosd.restservices.payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,20 +11,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    public EmployeeController(EmployeeRepository repository){
+    public EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler){
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/employees")
-    List<Employee> all(){
-        return  repository.findAll();
+    CollectionModel<EntityModel<Employee>> all(){
+//        List<EntityModel<Employee>> employees = repository.findAll().stream()
+//                .map(employee -> EntityModel.of(employee,
+//                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+//                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+//                .collect(Collectors.toList());
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+                .map(assembler::toModel) //
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
     // end::get-aggregate-root[]
 
@@ -33,8 +48,15 @@ public class EmployeeController {
 
     //Single item
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id){
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable Long id){
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return  assembler.toModel(employee);
+//        return EntityModel.of(employee,//
+//                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+//                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+//        );
+
     }
 
     @PutMapping("/employees/{id}")
--- Payroll/src/main/java/edu/oosd/restservices/payroll/EmployeeModelAssembler.java
+++ Payroll/src/main/java/edu/oosd/restservices/payroll/EmployeeModelAssembler.java
@@ -1,4 +1,18 @@
 package edu.oosd.restservices.payroll;
 
-public class EmployeeModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
 }
