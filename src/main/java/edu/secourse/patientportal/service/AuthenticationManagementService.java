a6e305bf81f6703dded88878b9f01c1d3c80f47b
Frederick Laud Amoah-Darko Jr. <freddie@Fredericks-MacBook-Pro-2.local>
2026-05-08 10:29:03 -0400

    cleaning repo.

--- src/main/java/edu/secourse/patientportal/service/AuthenticationManagementService.java
+++
@@ -1,8 +0,0 @@
-package edu.secourse.patientportal.service;
-
-import edu.secourse.patientportal.dto.LoginRequest;
-import edu.secourse.patientportal.dto.LoginResponse;
-
-public interface AuthenticationManagementService {
-    LoginResponse login(LoginRequest loginRequest);
-}
--- src/main/java/edu/secourse/patientportal/service/TokenService.java
+++
@@ -1,7 +0,0 @@
-package edu.secourse.patientportal.service;
-
-public interface TokenService {
-    String generateToken(String username, String role);
-    boolean validateToken(String token);
-    String extractUsername(String token);
-}
